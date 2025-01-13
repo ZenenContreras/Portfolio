94f204146c3620dd1a206bbb71b7a5ebc5e591f9
Zenen Contreras <zenencontreras1@gmail.com>
2025-01-13 08:34:46 -0500

    little changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Esto asegura que las rutas sean relativas en producción
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
